eb0ed26cd7cc10d79129b3aeb0ecab6916d903fd
Sahil Gupta <sahilsg0208@gmail.com>
2025-11-17 21:26:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const api = "https://api.quotable.io/random";
+
+async function getQuote(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+}
+
+getQuote(api_url);
